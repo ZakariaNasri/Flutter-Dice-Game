3319cffe6a833ec1afb1c0f598ed4c35d16b6e23
ZakariaNasri <zakaria.nasri@univconstantine2.dz>
2023-12-31 23:26:52 +0000

    All fonctions getting ready !

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,110 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(const DicePage());
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftNumber = 1;
+  int rightNumber = 1;
+  int total = 1;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.black87,
         appBar: AppBar(
           title: const Center(
             child: Text(
-              'Dicee',
+              'Dicee for Zax',
               style: TextStyle(color: Colors.white),
             ),
           ),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.grey[850],
         ),
-        body: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Expanded(
-                child: TextButton(
-                  onPressed: () {},
-                  child: Image.asset('images/dice1.png'),
-                ),
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/me.jpg'),
               ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () {},
-                  child: Image.asset('images/dice6.png'),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Zakaria Nasri',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600),
+              ),
+              Text(
+                'How Much Additional pull ups gonna take Today ?',
+                style: TextStyle(
+                    color: Colors.white70,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w400),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: TextButton(
+                      onPressed: () {
+                        setState(() {
+                          leftNumber = Random().nextInt(6) + 1;
+                          rightNumber = Random().nextInt(6) + 1;
+                          total = leftNumber + rightNumber;
+                        });
+                      },
+                      child: Image.asset('images/dice$leftNumber.png'),
+                    ),
+                  ),
+                  Expanded(
+                    child: TextButton(
+                      onPressed: () {
+                        setState(() {
+                          leftNumber = Random().nextInt(6) + 1;
+                          rightNumber = Random().nextInt(6) + 1;
+                          total = leftNumber + rightNumber;
+                        });
+                      },
+                      child: Image.asset('images/dice$rightNumber.png'),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 70,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white),
+                width: 250,
+                height: 70,
+                child: Center(
+                  child: Text(
+                    "You've got $total pull ups",
+                    style: TextStyle(
+                        color: Colors.grey[850],
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700),
+                  ),
                 ),
               ),
             ],
