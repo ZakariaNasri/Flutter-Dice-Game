cf5e00b624661643d2932b98be44e4bce13f48de
ZakariaNasri <zakaria.nasri@univconstantine2.dz>
2023-12-31 17:57:02 +0000

    Finishing the dice buttons , getting into functions !

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,44 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  return runApp(DicePage());
+  return runApp(const DicePage());
 }
 
 class DicePage extends StatelessWidget {
+  const DicePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: const Center(
+            child: Text(
+              'Dicee',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
           backgroundColor: Colors.red,
         ),
-        body: Container(
-          child: Image(image: AssetImage('images/dice1.png')),
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Expanded(
+                child: TextButton(
+                  onPressed: () {},
+                  child: Image.asset('images/dice1.png'),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {},
+                  child: Image.asset('images/dice6.png'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
